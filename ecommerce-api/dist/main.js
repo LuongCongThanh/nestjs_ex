3fce3bbfbab599083a43cca26875a7701752aa70
Luong Cong Thanh <thanhluong.it.0909@gmail.com>
2026-01-10 13:09:27 +0700

    done task 01: setup project

---
+++ ecommerce-api/dist/main.js
@@ -0,0 +1,33 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@nestjs/core");
+const app_module_1 = require("./app.module");
+const swagger_1 = require("@nestjs/swagger");
+const common_1 = require("@nestjs/common");
+const http_exception_filter_1 = require("./common/filters/http-exception.filter");
+const helmet_1 = require("helmet");
+async function bootstrap() {
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.use((0, helmet_1.default)());
+    app.useGlobalPipes(new common_1.ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+    }));
+    app.useGlobalFilters(new http_exception_filter_1.HttpExceptionFilter());
+    app.enableCors({
+        origin: process.env.CORS_ORIGIN?.split(',') || '*',
+        credentials: true,
+    });
+    const config = new swagger_1.DocumentBuilder()
+        .setTitle('Ecommerce API')
+        .setDescription('Production-ready ecommerce backend')
+        .setVersion('1.0')
+        .addBearerAuth()
+        .build();
+    const document = swagger_1.SwaggerModule.createDocument(app, config);
+    swagger_1.SwaggerModule.setup('api', app, document);
+    await app.listen(process.env.PORT || 3000);
+}
+bootstrap();
+//# sourceMappingURL=main.js.map
